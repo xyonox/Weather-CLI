b2fbdb638bd1444524fecbf74120ae3887583209
Xyonox <93388182+xyonox@users.noreply.github.com>
2026-08-12 13:19:56 +0200

    Update search logic: Added Structs and improved search functionality like choosing between multiple options and better formating

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/joho/godotenv"
@@ -17,6 +19,19 @@ const (
 	LocationApiServer                = "https://api.geoapify.com/v1/geocode/search?text=%v&format=json&apiKey=%v"
 	LocationApiServerWithCountryCode = "https://api.geoapify.com/v1/geocode/search?text=%v&filter=countrycode:%v&format=json&apiKey=%v"
 )
+
+type GeoapifyResponse struct {
+	Results []GeoapifyResult `json:"results"`
+}
+
+type GeoapifyResult struct {
+	Country          string  `json:"country"`
+	State            string  `json:"state"`
+	City             string  `json:"city"`
+	Latitude         float64 `json:"lat"`
+	Longitude        float64 `json:"lon"`
+	FormattedAddress string  `json:"formatted"`
+}
 
 func main() {
 	fmt.Println("---------------------- Weather CLI ----------------------")
@@ -50,11 +65,69 @@ func main() {
 
 	splits := strings.SplitN(strings.ToLower(input), ", ", 2)
 
+	var geoRespone *http.Response
+
 	if len(splits) == 2 {
-		fmt.Println(fmt.Sprintf(LocationApiServerWithCountryCode, splits[1], splits[0], os.Getenv("GEOAPIFY_API_KEY")))
+		url := fmt.Sprintf(LocationApiServerWithCountryCode, splits[1], splits[0], os.Getenv("GEOAPIFY_API_KEY"))
+		geoRespone, err = http.Get(url)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
 	} else {
-		fmt.Println(fmt.Sprintf(LocationApiServer, input, os.Getenv("GEOAPIFY_API_KEY")))
+		url := fmt.Sprintf(LocationApiServer, input, os.Getenv("GEOAPIFY_API_KEY"))
+		geoRespone, err = http.Get(url)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 	}
+
+	geoBody, err := io.ReadAll(geoRespone.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var geoapifyResponse GeoapifyResponse
+	var finallyResult GeoapifyResult
+
+	err = json.Unmarshal(geoBody, &geoapifyResponse)
+
+	fmt.Println("Found the following locations:")
+	for key, result := range geoapifyResponse.Results {
+		fmt.Printf("%v: %v\n", key, result.FormattedAddress)
+	}
+	if len(geoapifyResponse.Results) == 1 {
+		finallyResult = geoapifyResponse.Results[0]
+	} else if len(geoapifyResponse.Results) == 0 {
+		fmt.Println("\nNo results found")
+		return
+	} else if len(geoapifyResponse.Results) > 1 {
+		fmt.Println("\nMultiple results found, please select a location")
+		fmt.Print("> ")
+		if !scanner.Scan() {
+			fmt.Println("No input")
+		}
+		indexInput := strings.TrimSpace(scanner.Text())
+		index, err := strconv.Atoi(indexInput)
+		if err != nil {
+			fmt.Println("Invalid input")
+			return
+		}
+		if index < 0 || index >= len(geoapifyResponse.Results) {
+			fmt.Println("Invalid input")
+			return
+		}
+		t := geoapifyResponse.Results[index]
+		finallyResult = t
+	}
+
+	fmt.Println("\nSelected location:")
+	fmt.Printf("%v\nlat: %v\nlong: %v\n\n", finallyResult.FormattedAddress, finallyResult.Latitude, finallyResult.Longitude)
+
+	fmt.Println("Searching for weather:")
 
 	resp, err := http.Get(WeatherApiServer)
 	if err != nil {
