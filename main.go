e7862c12e78ff0881850fc8174ce8c431624fee9
Xyonox <93388182+xyonox@users.noreply.github.com>
2026-08-12 12:52:40 +0200

    Adding Location search

--- main.go
+++ main.go
@@ -1,23 +1,62 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
 )
 
 const (
-	ApiServer = "https://api.open-meteo.com/v1/forecast?latitude=48.1374&longitude=11.5755&current=temperature_2m"
+	WeatherApiServer                 = "https://api.open-meteo.com/v1/forecast?latitude=48.1374&longitude=11.5755&current=temperature_2m"
+	LocationApiServer                = "https://api.geoapify.com/v1/geocode/search?text=%v&format=json&apiKey=%v"
+	LocationApiServerWithCountryCode = "https://api.geoapify.com/v1/geocode/search?text=%v&filter=countrycode:%v&format=json&apiKey=%v"
 )
 
 func main() {
 	fmt.Println("---------------------- Weather CLI ----------------------")
 
 	// TODO Wetter API und sich einfach wetter von einer vorbestimmten Stadt zu holen
-	// TODO Automatisch überm Standort die Stadt bestimmen
-	// TODO über Args eine Stadt bestimmen lassen
+	// TODO Automatisch überm Standort die Stadt bestimmen?
+	// TODO über Args eine Stadt bestimmen lassen?
 
-	resp, err := http.Get(ApiServer)
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found")
+	}
+	apiKey := os.Getenv("GEOAPIFY_API_KEY")
+	if apiKey == "" {
+		fmt.Println("GEOAPIFY_API_KEY is not set")
+		return
+	}
+
+	fmt.Println("Enter a location: ")
+	fmt.Print("> ")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	if !scanner.Scan() {
+		fmt.Println("No input")
+		return
+	}
+
+	input := strings.TrimSpace(scanner.Text())
+
+	fmt.Println("Searching for: ", input)
+
+	splits := strings.SplitN(strings.ToLower(input), ", ", 2)
+
+	if len(splits) == 2 {
+		fmt.Println(fmt.Sprintf(LocationApiServerWithCountryCode, splits[1], splits[0], os.Getenv("GEOAPIFY_API_KEY")))
+	} else {
+		fmt.Println(fmt.Sprintf(LocationApiServer, input, os.Getenv("GEOAPIFY_API_KEY")))
+	}
+
+	resp, err := http.Get(WeatherApiServer)
 	if err != nil {
 		fmt.Println(err)
 		return
